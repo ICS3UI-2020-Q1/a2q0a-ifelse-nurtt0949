5f3e1fda37da69a896ded4423fa0e521aa4261ee
nurtt0949 <nurtt0949@wrdsb.ca>
2020-09-15 13:40:14 +0000

    Driving age

--- Main.java
+++ Main.java
@@ -1,6 +1,9 @@
+import java.util.Scanner;
+
+
 /**
  *
- * @author 
+ * @author Tolga Nurtekin
  */
 public class Main {
 
@@ -9,7 +12,30 @@ public class Main {
    * @param args the command line arguments
    */
   public static void main(String[] args) {
-    // TODO code application logic here
-    
+    //create a Scanner for user input
+   Scanner input = new Scanner(System.in);
+
+   //define minimum age
+   final int MIN_DRIVER_AGE = 16;
+   
+   //prompt user to enter arguments
+   System.out.println("Please enter your age:");
+
+   //get the age from the user
+   int age = input.nextInt();
+
+  //determine if they can drive
+  if(age >= MIN_DRIVER_AGE){
+ 
+ //tell them they can drive
+ System.out.println("You can legally drive");
+
+  } else {
+
+  //not old enough
+  System.out.println("Sorry, you can't legally drive");
+
+
+  }  
   }
 }
